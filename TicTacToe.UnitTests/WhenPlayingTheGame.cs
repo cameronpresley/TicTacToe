c5cc7ad00dd162ba859475c0be559021184f8512
Cameron Presley <Cameron@TheSoftwareMentor.com>
2020-04-16 08:26:05 -0400

    Added more tests on the game loop.

--- TicTacToe.UnitTests/WhenPlayingTheGame.cs
+++ TicTacToe.UnitTests/WhenPlayingTheGame.cs
@@ -1,5 +1,6 @@
 using TicTacToe;
 using NUnit.Framework;
+using System;
 
 namespace TicTacToe.UnitTests
 {
@@ -19,6 +20,30 @@ namespace TicTacToe.UnitTests
             var game = new Game();
 
             game.FillLocation(0, 0);
+
+            Assert.IsTrue(game.CurrentPlayer.Symbol == 'O');
+        }
+
+        [Test]
+        public void AndTheBoardIsFilledWithNoWinnerThenTheGameIsOver()
+        {
+            /*
+                    X | O | X
+                    X | O | X
+                    O | X | O
+            */
+            var game = new Game();
+            game.FillLocation(0, 0);
+            game.FillLocation(0, 1);
+            game.FillLocation(0, 2);
+            game.FillLocation(1, 1);
+            game.FillLocation(1, 0);
+            game.FillLocation(2, 0);
+            game.FillLocation(2, 1);
+            game.FillLocation(2, 2);
+            game.FillLocation(1, 2);
+
+            Assert.IsTrue(game.IsGameOver());
         }
     }
 }
